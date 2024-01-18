c25351290f8efbd98076a093c4e53c1b6ed1b920
leohb07 <leohb1501@gmail.com>
2024-01-18 11:42:18 -0300

    feat: add signup route

--- src/shared/modules/infra/routes.tsx
+++ src/shared/modules/infra/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { DashboardContainer } from '@/subdomains/app/home/containers/dashboard.container'
 import { SignInContainer } from '@/subdomains/auth/containers/signin.container'
+import { SignUpContainer } from '@/subdomains/auth/containers/signup.container'
 
 import { AppLayout } from '../_layouts/app.layout'
 import { AuthLayout } from '../_layouts/auth.layout'
@@ -16,6 +17,9 @@ export const routes = createBrowserRouter([
 	{
 		path: '/',
 		element: <AuthLayout />,
-		children: [{ path: '/sign-in', element: <SignInContainer /> }],
+		children: [
+			{ path: '/sign-in', element: <SignInContainer /> },
+			{ path: '/sign-up', element: <SignUpContainer /> },
+		],
 	},
 ])
