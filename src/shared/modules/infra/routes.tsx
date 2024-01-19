517760b3582a1bd21ce792b02a550d5d06e19de8
leohb07 <leohb1501@gmail.com>
2024-01-19 11:12:52 -0300

    feat: add not found page

--- src/shared/modules/infra/routes.tsx
+++ src/shared/modules/infra/routes.tsx
@@ -7,11 +7,13 @@ import { SignUpContainer } from '@/subdomains/auth/containers/signup.container'
 
 import { AppLayout } from '../_layouts/app.layout'
 import { AuthLayout } from '../_layouts/auth.layout'
+import { NotFound } from '../_layouts/not-found.layout'
 
 export const routes = createBrowserRouter([
 	{
 		path: '/',
 		element: <AppLayout />,
+		errorElement: <NotFound />,
 		children: [
 			{ path: '/', element: <DashboardContainer /> },
 			{ path: '/orders', element: <OrdersContainer /> },
